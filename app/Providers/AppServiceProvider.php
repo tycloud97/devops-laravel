e5d0e6d4431d6d749180c5307b86bff74e25f448
Ty <tycloud97@gmail.com>
2022-08-07 00:05:05 +0700

    commit coide

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,8 +13,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (App::environment('production')) {
-            URL::forceScheme('https');
+        if (env('APP_ENV') === 'production') {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
         }
     }
 
