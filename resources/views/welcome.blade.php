063ba5c9483970b86daa580c84bcce7d7a5881da
Ty <tycloud97@gmail.com>
2022-08-07 14:59:39 +0700

    commit code

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Ty Test Laravel CI/CD
+                    Ty Demo Laravel CI/CD
                 </div>
                 
                 <div class="m-b-md">
